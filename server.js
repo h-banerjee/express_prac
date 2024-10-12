53666a6c53c4aa8db9047ac1c5ccf7435e0a0be2
h-banerjee <harshbanerjee30@gmail.com>
2024-10-12 13:07:55 +0530

    added some helpful comments

--- server.js
+++ server.js
@@ -6,36 +6,36 @@ app.get("/",(req,res)=>{
 })
     //  multiply server
 app.get("/multiply",(req,res)=>{
-    let a=req.query.a;
-    let b=req.query.b;
-    let result = a*b;
-    res.json({
-        result:result
+    let a=req.query.a; // extracts the query parameter "a" from the request parameter 
+    let b=req.query.b; // extracts the query parmeter "b" frmo the request parameter
+    let result = a*b; //multiples "a" and "b"
+    res.json({        // sends JSON response with the "result"
+        result:result // the result is returned in JSON format
     })
 })
 // addtion server
 
 app.get("/addition",(req,res)=>{
-    let a = parseInt(req.query.a);
-    let b = parseInt(req.query.b);
-    let result = a+b;
+    let a = parseInt(req.query.a);     // Extracts and converts 'a' to an integer
+    let b = parseInt(req.query.b);     // Extracts and converts 'b' to an integer
+    let result = a+b;                   // Adds 'a' and 'b'
     res.send(result.toString());        // it gives only string formate value
 })
 // substraction server
 app.get("/substraction",(req,res)=>{
-    let a =parseInt(req.query.a);
-    let b= ParseInt(req.query.b);
-    let result = a-b;
-    res.send(result.toString());
+    let a =parseInt(req.query.a);  // Parse 'a' parameter from query string to an integer
+    let b= ParseInt(req.query.b);// Parse 'b' parameter from query string to an integer 
+    let result = a-b; // Perform subtraction of 'a' and 'b'
+    res.send(result.toString()); // Send the result as a string in the response
 }
 )
 //division server
 app.get("/division",(req,res)=>{
-    let a = req.query.a;
-    let b = req.query.b;
-    let result = a/b;
+    let a = req.query.a; // Get 'a' from the query string
+    let b = req.query.b; // Get 'b' from the query string
+    let result = a/b; // Perform division of 'a' by 'b'
     res.json({
-        ans:result
+        ans:result // Return the result in a JSON response with key 'ans'
     })
 })
 
